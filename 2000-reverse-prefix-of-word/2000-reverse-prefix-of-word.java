b6aebb1f0fe03f43c4e66a7c14aa842ff4f1e0dd
Jyoshnap52 <jyoshnap52@gmail.com>
2026-08-25 15:22:02 +0530

    Time: 1 ms (47.47%), Space: 43 MB (55.18%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+
+        if (index == -1) return word;
+
+        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+        sb.reverse();
+
+        return sb + word.substring(index + 1);
+    }
+}
